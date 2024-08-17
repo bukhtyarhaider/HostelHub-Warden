23fb39104a68ed9e8fd06a2b3abbd6eb47d33226
Mukhtar Hussain <mukhtar.hussai9@gmail.com>
2024-08-17 08:43:32 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./main.scss";
+
+function App() {
+  return <div className="app-container">Home</div>;
+}
+
+export default App;
---
+++ src/assets/index.ts
@@ -0,0 +1,2 @@
+export { default as logo } from "./icons/logo.svg";
+export { default as avatar } from "./images/avatar.png";
