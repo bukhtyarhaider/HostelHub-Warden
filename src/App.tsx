01d2e4db0fcc68ac545dfe614a15e99ddcaf5b75
Mukhtar Hussain <mukhtar.hussai9@gmail.com>
2024-09-11 20:44:59 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,9 @@
 import "./main.scss";
-import { Routes, Route, useLocation } from "react-router-dom";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Header from "./components/Header/Header";
-import routes from "./routes";
-import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  const location = useLocation();
-
   return (
     <div className="app-container">
-      {location.pathname !== "/login" && location.pathname !== "/register" && (
-        <Sidebar />
-      )}
-
-      <div className="main-content">
-        {location.pathname !== "/login" &&
-          location.pathname !== "/register" && <Header />}
-        <Routes>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={
-                route.path === "/login" ? (
-                  <route.element />
-                ) : (
-                  <PrivateRoute element={route.element} />
-                )
-              }
-            />
-          ))}
-          <Route
-            path="*"
-            element={
-              <PrivateRoute
-                element={routes.find((route) => route.path === "/")?.element}
-              />
-            }
-          />
-        </Routes>
-      </div>
+      <h2>Home</h2>
     </div>
   );
 }
